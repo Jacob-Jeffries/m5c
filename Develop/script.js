485869c90e71484eaaa67813fe5d84d0724f7695
Jacob-Jeffries <jeffrij85@gmail.com>
2023-01-02 15:43:26 -0500

    Created the listen function on the save button, and store the value of the specific text area to local storage as per its parent, div, id

--- Develop/script.js
+++ Develop/script.js
@@ -5,16 +5,14 @@
 $(function () {
   let currentTime = document.querySelector('#currT');
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-
+  //The code snippet below listens for a click on each save button, then gets the parent ID of the specific button clicked and saves the text to the local storage
   $(this).on('click', '.btn', function(){
     let hour = $(this.parentElement);
     console.log(hour[0].id);
+    let id = hour[0].id;
+    todoText = document.body.children[1].children[id].children[1].value;
+    console.log(todoText);
+    localStorage.setItem(id, todoText);
   });
 
 
@@ -41,8 +39,10 @@ $(function () {
   for(let x=0; x<24; x++){
     let todo = localStorage.getItem(x);
     if(todo){
+      console.log(x);
+      console.log(todo);
       let textArea = document.body.children[1].children[x].children[1]
-      todo.innerText = textArea;
+      textArea.innerText = todo;
     };
   };  
 
