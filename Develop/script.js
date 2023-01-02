d69af8f505fe08ae1d9f6a0a537b8ee9f5426f2f
Jacob-Jeffries <jeffrij85@gmail.com>
2023-01-02 13:48:30 -0500

    Cleaned up the comments and structure of my code from yesterday.

--- Develop/script.js
+++ Develop/script.js
@@ -11,13 +11,8 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
 
+  //The code snippet below compares the current hour to the ID's of each hour slot on the web app and changes the color based on the time: past, present, future
   setInterval(function () {
     let currHour = dayjs().format('H');
 
@@ -41,7 +36,7 @@ $(function () {
   // attribute of each time-block be used to do this?
   
 
-  // TODO: Add code to display the current date in the header of the page.
+  //The code snippet below pings the system time every 500ms and displays it on the web app
   setInterval(function() {
     currentTime.innerText = dayjs().format('MMM DD, YYYY [at] hh:mm:ssA');
   }, 500);
